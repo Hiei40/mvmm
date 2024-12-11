e237593996ab19516322a859e2f3bad740123067
Hiei40 <k.abdalrhem@gmail.com>
2024-12-11 18:54:54 +0200

    11

--- lib/feature/home/presentaion/views/widgets/books_details_view_body.dart
+++ lib/feature/home/presentaion/views/widgets/books_details_view_body.dart
@@ -1,18 +1,44 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import '../../../../../core/utils/styles.dart';
 import 'custom_book_details_app_bar.dart';
+import 'custom_book_image.dart';
+
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 30),
+    var width = MediaQuery.of(context).size.width;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
         children: [
-          CustomBookDetailsAppBar(),
-        ],
+          const CustomBookDetailsAppBar(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: width * .17),
+            child: const CustomBookImage(),
+          ),
+          const SizedBox(
+            height: 43,
+          ),
+          Text("The Jungle Book",
+            style: Styles.textStyle30.copyWith(
+                fontWeight: FontWeight.bold),),
+       const SizedBox(
+         height: 6,
+       ),
+       Opacity(opacity: .7,
+
+       child: Text("Rudyard Kipling",
+        style: Styles.textStyle18.copyWith(
+          fontStyle: FontStyle.italic,
+          fontWeight: FontWeight.w500
+        ),
+       )
+       ),
+      ],
       ),
     );
   }
