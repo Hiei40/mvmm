809c73dd5ca333b8abaac5182b3335338bc30c8e
Hiei40 <k.abdalrhem@gmail.com>
2024-12-12 16:33:27 +0200

    fetchNewsetBooks

--- lib/Features/home/data/repo/home_repo_imp.dart
+++ lib/Features/home/data/repo/home_repo_imp.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 import 'package:mvmm/Features/home/data/models/book_model/book.dart';
 import 'package:mvmm/Features/home/data/repo/home_repo.dart';
 import 'package:mvmm/core/errors/faliure.dart';
@@ -21,18 +22,49 @@ class HomeRepoImp implements HomeRepo {
       return Right(books);
     }
     catch (e) {
-      return Left(ServerFaliure(e.toString()));
+      if(e is DioError){
+        return Left(ServerFailure.fromDioError(e));
+      }
+      return Left(ServerFailure(e.toString()));
     }
   }
 
 
   @override
-  Future<Either<Failure, List<Bookmodel>>> fetchFeaturedBooks() {
+  Future<Either<Failure, List<Bookmodel>>> fetchFeaturedBooks() async {
+    try {
+      var data = await apiservice.get(
+          endpoint: 'volumes?Filtering=free-ebooks&q=subject:Programming');
+      List<Bookmodel> books = [];
+      for (var item in data['items']) {
+        books.add(Bookmodel.fromJson(item));
+      }
 
-
-
-
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+      return right(books);
+    } catch (e) {
+      if (e is DioError) {
+        return left(
+          ServerFailure.fromDioError(e),
+        );
+      }
+      return left(
+        ServerFailure(
+          e.toString(),
+        ),
+      );
+    }
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
